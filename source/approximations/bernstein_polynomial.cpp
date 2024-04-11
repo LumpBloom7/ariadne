6aae60133d243bd7106ea471c65fe7002fc33323
Derrick Timmermans <derrick.timmermans@outlook.com>
2024-04-11 12:39:59 +0200

    Apply some formatting

--- source/approximations/bernstein_polynomial.cpp
+++ source/approximations/bernstein_polynomial.cpp
@@ -2,36 +2,45 @@
 
 namespace Ariadne {
 
-BernsteinPolynomial::BernsteinPolynomial(const std::function<double(double)>& function, int degree) : _function {function}, _degree {degree} {
+BernsteinPolynomial::BernsteinPolynomial(const std::function<double(double)>& function, int degree) : _function{function}, _degree{degree}
+{
     _coefficients = computeCoefficients();
     _binomialCache = {};
 }
 
-FloatMPBounds BernsteinPolynomial::factorial(int x) {
+FloatMPBounds BernsteinPolynomial::factorial(int x)
+{
     // This factorial function uses a cache to avoid recomputation of factors.
     // *In theory* all generated factorials should be 100% correct as I make sure that more than enough bits are reserved for the mantissa.
     // The cache is global, so all instances of BernsteinPolynomials will refer to the same cache.
 
-    if(_factorialCache.size() > x)
+    if (_factorialCache.size() > x)
         return _factorialCache[x];
 
-    auto res = factorial(x - 1);
+    auto res = _factorialCache.back();
 
-    _mantissaBitsCount += log2(x);
-    int numbits = static_cast<int> ((_mantissaBitsCount));
+    // Recursion causes a stackoverflow on higher x values
+    // For-loop my beloved gets to shine again.
+    for (int i = _factorialCache.size(); i <= x; ++i) {
+        _mantissaBitsCount += log2(i);
+        int numbits = static_cast<int>((_mantissaBitsCount) + 1);
 
-    res = FloatMPBounds(res, MultiplePrecision(numbits)) * x;
+        res = FloatMPBounds(res, MultiplePrecision(numbits)) * i;
 
-    std::cout << res << res.precision() << std::endl;
+        std::cout << res << res.precision() << std::endl;
 
-    return _factorialCache.emplace_back(res);
+        _factorialCache.emplace_back(res);
+    }
+
+    return res;
 }
 
-FloatMPBounds BernsteinPolynomial::binomialCoefficient(const int n, const int k) {
+FloatMPBounds BernsteinPolynomial::binomialCoefficient(const int n, const int k)
+{
     // Like the factorial function, this one uses a cache as well. The precision should always be larger-than or equal to what is actually needed.
     // This is stored not on a global level though.
 
-    if(_binomialCache.size() > k)
+    if (_binomialCache.size() > k)
         return _binomialCache[k];
 
     // FloatMP preserves precision of the least precise factor during multiplication of two FloatMPs
@@ -42,27 +51,31 @@ FloatMPBounds BernsteinPolynomial::binomialCoefficient(const int n, const int k)
     return _binomialCache.emplace_back(a / (b * c));
 }
 
-std::vector<ExactDouble> BernsteinPolynomial::computeCoefficients() {
+std::vector<ExactDouble> BernsteinPolynomial::computeCoefficients()
+{
     std::vector<ExactDouble> coefficients = std::vector<ExactDouble>(_degree + 1);
 
-    for(size_t i = 0; i <= _degree; ++i)
+    for (size_t i = 0; i <= _degree; ++i)
         coefficients[i] = ExactDouble(_function(i / static_cast<float>(_degree)));
 
     return coefficients;
 }
 
-FloatMPBounds BernsteinPolynomial::bernsteinBasisPolynomialFor(int v, double x, size_t effort) {
+FloatMPBounds BernsteinPolynomial::bernsteinBasisPolynomialFor(int v, double x, size_t effort)
+{
     auto X = FloatMPBounds(exact(x), MultiplePrecision(effort));
 
     return binomialCoefficient(_degree, v) * pow(X, v) * pow(1 - X, _degree - v);
 }
 
-FloatMPBounds BernsteinPolynomial::evaluate(double x, size_t effort) {
+FloatMPBounds BernsteinPolynomial::evaluate(double x, size_t effort)
+{
     FloatMPBounds sum = FloatMPBounds(exact(0), MultiplePrecision(effort));
 
-    if(x < 0 || x > 1) return sum;
+    if (x < 0 || x > 1)
+        return sum;
 
-    for(size_t i = 0; i <= _degree; ++i) {
+    for (size_t i = 0; i <= _degree; ++i) {
         auto bp = bernsteinBasisPolynomialFor(i, x, effort);
         sum += _coefficients[i] * bp;
     }
@@ -73,4 +86,4 @@ FloatMPBounds BernsteinPolynomial::evaluate(double x, size_t effort) {
 std::vector<FloatMPBounds> BernsteinPolynomial::_factorialCache = {FloatMPBounds(exact(1), MultiplePrecision(1)), FloatMPBounds(exact(1), MultiplePrecision(1))};
 double BernsteinPolynomial::_mantissaBitsCount = log2(1);
 
-}
+} // namespace Ariadne
--- source/approximations/bernstein_polynomial.hpp
+++ source/approximations/bernstein_polynomial.hpp
@@ -1,30 +1,29 @@
 #ifndef ARIADNE_BERNSTEIN_POLYNOMIAL_HPP
 #define ARIADNE_BERNSTEIN_POLYNOMIAL_HPP
 
-#include "utility/standard.hpp"
-#include <functional>
 #include <cmath>
+#include <functional>
 #include <iostream>
-#include "numeric/int.hpp"
 
-#include "numeric/floatmp.hpp"
 #include "numeric/bits.hpp"
-#include "numeric/float_bounds.hpp"
-#include "numeric/rounding.hpp"
-#include "numeric/real.hpp"
-#include "numeric/validated_real.hpp"
 #include "numeric/dyadic.hpp"
+#include "numeric/float_bounds.hpp"
+#include "numeric/floatmp.hpp"
+#include "numeric/int.hpp"
 #include "numeric/rational.hpp"
-
+#include "numeric/real.hpp"
+#include "numeric/rounding.hpp"
+#include "numeric/validated_real.hpp"
+#include "utility/standard.hpp"
 
 namespace Ariadne {
 
 class BernsteinPolynomial {
-public:
+  public:
     BernsteinPolynomial(const std::function<double(double)>& function, int degree);
     FloatMPBounds evaluate(double x, size_t effort);
 
-private:
+  private:
     FloatMPBounds binomialCoefficient(const int n, const int k);
     std::vector<ExactDouble> computeCoefficients();
     FloatMPBounds bernsteinBasisPolynomialFor(int v, double x, size_t effort);
@@ -41,11 +40,12 @@ private:
     static double _mantissaBitsCount;
     std::vector<FloatMPBounds> _binomialCache;
 
-    static FloatMPBounds toPrecision(const FloatMPBounds& x, const MultiplePrecision& pr){
+    static FloatMPBounds toPrecision(const FloatMPBounds& x, const MultiplePrecision& pr)
+    {
         return FloatMPBounds(x, pr);
     }
 };
 
-}
+} // namespace Ariadne
 
 #endif // ARIADNE_BERNSTEIN_POLYNOMIAL_HPP

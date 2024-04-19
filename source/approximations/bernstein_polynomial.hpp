b788f5c7699d019f02397de4d43092cfb679b74a
Derrick Timmermans <derrick.timmermans@outlook.com>
2024-04-19 20:06:21 +0200

    Avoid dangling reference in ctor

--- source/approximations/bernstein_polynomial.hpp
+++ source/approximations/bernstein_polynomial.hpp
@@ -9,13 +9,14 @@
 #include "numeric/float_bounds.hpp"
 #include "numeric/floatmp.hpp"
 #include "numeric/integer.hpp"
+#include "numeric/real.hpp"
+#include "numeric/validated_real.hpp"
 #include "utility/cache.hpp"
 #include "utility/factorials.hpp"
 #include "utility/hash.hpp"
 #include "utility/hash_numeric.hpp"
 #include "utility/hash_tuple.hpp"
 #include "utility/standard.hpp"
-
 namespace Ariadne {
 
 class BernsteinPolynomialBase {
@@ -24,18 +25,23 @@ class BernsteinPolynomialBase {
 };
 
 template<typename T>
-    requires HasPrecisionType<T> && ConvertibleTo<T, Bounds<T>>
 class BernsteinPolynomial : protected BernsteinPolynomialBase {
+    typedef typename T::RoundingModeType RND;
+    typedef typename T::PrecisionType PR;
+
   public:
-    BernsteinPolynomial(const std::function<Bounds<T>(Bounds<T>)>& function, int degree, size_t max_precision) : _function{function}, _degree{degree}, _precision{max_precision} {
-        _coefficients = computeCoefficients();
-    }
+    BernsteinPolynomial(std::function<Bounds<T>(Bounds<T>)> function, int degree) : _function{function}, _degree{degree} {}
 
     Bounds<T> evaluate(Bounds<T> x) {
-        Bounds<T> sum = T(0, x.precision());
+        // This is out of evaluation domain, return 0.
+        // We could remove this with little consequence to correctness, as evaluating a BernsteinPolynomial like this would be undefined anyways.
+        // Doing this allows us to skip computations though.
+        if (Detail::possibly((x < 0 || x > 1).repr()))
+            return T(0, x.precision());
 
-        if (Detail::possibly((x < 0 ||x > 1).repr()))
-            return sum;
+        validateCoefficients(x.precision());
+
+        auto sum = Bounds<T>(x.precision());
 
         for (size_t i = 0; i <= _degree; ++i) {
             auto bp = bernsteinBasisPolynomialFor(i, x);
@@ -46,25 +52,73 @@ class BernsteinPolynomial : protected BernsteinPolynomialBase {
     };
 
   private:
-    std::vector<Bounds<T>> computeCoefficients() {
-        std::vector<Bounds<T>> coefficients = {};
+    void validateCoefficients(PR precisionType) {
+        // Coefficients are sufficient for the desired evaluation precision. Avoid recomputation.
+        if (!_coefficients.empty() && _coefficients[0].precision() >= precisionType)
+            return;
 
-        for (size_t i = 0; i <= _degree; ++i)
-            coefficients.emplace_back(_function(T(Approximation<T>(i / static_cast<float>(_degree), MultiplePrecision(_precision)))));
+        _coefficients = {};
 
-        return coefficients;
+        for (size_t i = 0; i <= _degree; ++i) {
+            auto res = _function(Bounds<T>(T(Approximation<T>(i / static_cast<float>(_degree), precisionType))));
+
+            _coefficients.emplace_back(res);
+        }
     };
 
     Bounds<T> bernsteinBasisPolynomialFor(int v, Bounds<T> x) {
         return binomialCoefficients(_degree, v) * pow(x, v) * pow(1 - x, _degree - v);
     }
 
-    const std::function<Bounds<T>(Bounds<T>)>& _function;
+    std::function<Bounds<T>(Bounds<T>)> _function;
     const int _degree;
     std::vector<Bounds<T>> _coefficients;
-    const size_t _precision;
 };
 
+// TODO: We can't approximate easily using Reals, as it would require several computations of the original functions, which is not ideal
+// I want to cache the coefficient as Either a validated real, or a dyadic bounds. But Reals don't support math operations with those types straightforward.
+
+/*
+template<>
+class BernsteinPolynomial<Real> : protected BernsteinPolynomialBase {
+  public:
+    BernsteinPolynomial(const std::function<Real(Real)>& function, int degree, size_t max_precision) : _function{function}, _degree{degree}, _precision{max_precision} {
+        _coefficients = computeCoefficients();
+    }
+
+    DyadicBounds evaluate(Real x) {
+        DyadicBounds sum = DyadicBounds(0);
+
+        for (size_t i = 0; i <= _degree; ++i) {
+            auto bp = bernsteinBasisPolynomialFor(i, x);
+            sum += _coefficients[i] * bp;
+        }
+
+        return sum;
+    };
+
+  private:
+    std::vector<Bounds<Dyadic>> computeCoefficients() {
+        std::vector<Bounds<Dyadic>> coefficients = {};
+
+        auto D = Real(_degree);
+
+        for (size_t i = 0; i <= _degree; ++i) {
+            coefficients.emplace_back(_function(Real(i) / D).compute(Effort(_precision)));
+        }
+
+        return coefficients;
+    };
+
+    Real bernsteinBasisPolynomialFor(int v, Real x) {
+        return binomialCoefficients(_degree, v) * pow(x, v) * pow(1 - x, _degree - v);
+    }
+
+    const std::function<Real(Real)>& _function;
+    const int _degree;
+    std::vector<Bounds<Dyadic>> _coefficients;
+    const size_t _precision;
+}; */
 } // namespace Ariadne
 
 #endif // ARIADNE_BERNSTEIN_POLYNOMIAL_HPP
